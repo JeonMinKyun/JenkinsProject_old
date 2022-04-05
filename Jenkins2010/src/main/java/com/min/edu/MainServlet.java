0bf53c3739a76420bb68ef267eb6bb53c1b9ace7
lucas0429 <lucas0429@gmail.com>
2022-04-05 17:41:04 +0900

    deGet만듬

--- Jenkins2010/src/main/java/com/min/edu/MainServlet.java
+++ Jenkins2010/src/main/java/com/min/edu/MainServlet.java
@@ -1,9 +1,17 @@
 package com.min.edu;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class MainServlet extends HttpServlet{
 
 	private static final long serialVersionUID = 5653276473058107380L;
 
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	}
 }
